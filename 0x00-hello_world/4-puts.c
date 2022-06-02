765e988ac4be56e70e12439cd345a1b1e4d70b6f
mayluv17 <mayowaibrahim009@gmail.com>
2022-06-02 07:19:15 -0700

    some C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* main function prints some strings*/
 /**
  * main - print where we begin
- * Return:  return zero to declare success
+ * Return:  return zero to declare sucess
  */
 int main(void)
 {
