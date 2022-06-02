98b0eb13df912eb7444cac21df5e885ca52012e7
mayluv17 <mayowaibrahim009@gmail.com>
2022-06-02 11:38:55 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - where it goes down
+ * Printf: prints some strings
+ * Return: return zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
