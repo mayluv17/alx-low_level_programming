8c4da4207ccc743ae8e2bb4e50e47468b2f3bb26
mayluv17 <mayowaibrahim009@gmail.com>
2022-06-02 06:35:02 -0700

    some C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main (void)
+/* main function prints some strings*/
+/**
+ * main - print where we begin
+ * return - return 0
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
