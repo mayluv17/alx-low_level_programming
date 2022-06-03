a88234f3b95c8f193db43fa68b41a1ac0671cfec
mayluv17 <mayowaibrahim009@gmail.com>
2022-06-03 12:06:07 -0700

    a-z loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,10 @@
  * main - where it all started
  * Putchar: Print some char
  * Return: return 0 if success
- * */
+ */
 
-int main (void)
-{	
+int main(void)
+{
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
