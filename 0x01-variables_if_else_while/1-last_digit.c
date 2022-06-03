7da8b622ef6b88c3942cfa963d4f3e45c9e05fd8
mayluv17 <mayowaibrahim009@gmail.com>
2022-06-03 08:59:29 -0700

    if else variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,17 @@ int main(void)
 	int n;
 	int l;
 	char str[50];
-	int l = n % 10;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	l = n % 10;
+	
 	if (l > 5)
 	{
 		char str[50] = "is greater than 5";
 	} else if (l == 0)
 	{
 		char str[50] = "is 0";
-	} else if (n < 6 && n != 0)
+	} else if (n < 6i)
 	{
 		char str[50] = "is less than 6 and not 0";
 	} else
